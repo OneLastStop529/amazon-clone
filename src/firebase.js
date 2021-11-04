cdbeff00181ff4cce15c5675b2db413982929ba0
yize <hugohayes123@outlook.com>
2021-11-04 09:07:10 -0700

    Added Header  frontend module, added MUI & firebase dependency

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCmLj4GAnxt64gjKFTUPqYWbkTEtzdMj88",
+    authDomain: "cl-a07a7.firebaseapp.com",
+    projectId: "cl-a07a7",
+    storageBucket: "cl-a07a7.appspot.com",
+    messagingSenderId: "81096597064",
+    appId: "1:81096597064:web:e799235484715b68d4c27d",
+    measurementId: "G-XZF9ZRKRY9"
+  };
