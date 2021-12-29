7b653e8b2beb7a8cef42ee353ee8f0c7e5314e3f
yize <hugohayes123@outlook.com>
2021-12-28 16:56:03 -0800

    added Remove from basket function

--- src/Subtotal/Subtotal.js
+++ src/Subtotal/Subtotal.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './Subtotal.css'
+import { useStateValue } from "../StateProvider";
+import { getBasketTotal } from "../reducer"
 import CurrencyFormat from 'react-currency-format'
 
 function Subtotal() {
+    const [{basket}, dispatch] = useStateValue();
     return (
         <div className="subtotal">
             <CurrencyFormat renderText={(value) => (
             <>
                 <p>
-                    Subtotal (0 items): <strong> 0 </strong>
+                    Subtotal ({basket.length} items): <strong> {value} </strong>
                 </p>
 
                 <small className="subtotal__gift">
@@ -18,7 +21,7 @@ function Subtotal() {
             </>
             )}
             decimalScale = {2}
-            value = {0}
+            value = {getBasketTotal(basket)}
             displayType={"text"}
             thousandSeparator={true}
             prefix={"$"}
